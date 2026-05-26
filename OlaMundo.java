6af9e3c7b1185f9cbd2f7d05903ff07d6182118c
Manoel Amancio <manoel.amanciio@gmail.com>
2026-05-26 20:35:55 -0300

    Entrada de dados pelo terminal

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class OlaMundo {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();    
+
+        sc.close();
+        System.out.printf("Olá %s!", nome);   
     }
 }
