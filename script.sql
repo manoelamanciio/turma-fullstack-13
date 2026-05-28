b6ea59d5725b7d93f545231b1035015514c64423
Manoel Amancio <manoel.amanciio@gmail.com>
2026-05-28 19:56:32 -0300

    Criacao da tabela de alunos

--- script.sql
+++ script.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE treinarecife_db;
+
+CREATE TABLE treinarecife_db.tb_alunos (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    sobrenome VARCHAR(150) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    data_nascimento DATE
+);
