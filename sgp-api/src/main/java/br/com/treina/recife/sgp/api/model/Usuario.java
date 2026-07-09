5d9a354582fda48b539afaded31d676d1a2ade8b
Manoel <manoel.amanciio@gmail.com>
2026-07-09 10:08:04 -0300

    Adicionando atributos a classe Usuario

--- sgp-api/src/main/java/br/com/treina/recife/sgp/api/model/Usuario.java
+++ sgp-api/src/main/java/br/com/treina/recife/sgp/api/model/Usuario.java
@@ -1,5 +1,12 @@
 package br.com.treina.recife.sgp.api.model;
 
+import java.time.LocalDate;
+
 public class Usuario {
+
+    private String nome;
+    private Integer Id;
+    private String cpf;
+    private LocalDate dataNascimento;
 
 }
