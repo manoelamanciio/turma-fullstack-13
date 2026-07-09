28c6bf86d46edb4750e27081daf5d517ac8feaa5
Manoel <manoel.amanciio@gmail.com>
2026-07-09 10:32:01 -0300

    Adicionando metodos, construtores e atributos

--- sgp-api/src/main/java/br/com/treina/recife/sgp/api/model/Usuario.java
+++ sgp-api/src/main/java/br/com/treina/recife/sgp/api/model/Usuario.java
@@ -2,10 +2,27 @@ package br.com.treina.recife.sgp.api.model;
 
 import java.time.LocalDate;
 
-public class Usuario {
+import javax.annotation.processing.Generated;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String nome;
-    private Integer Id;
+    @Column
+    private Long Id;
     private String cpf;
     private LocalDate dataNascimento;
 
