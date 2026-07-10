bfee612ed02926868b325c100a60e70f26a098ae
Deivid Marques <deividmarqs@gmail.com>
2026-07-09 21:34:20 -0300

    Adicionando três classes: UsuarioRepository, ProjetoRepository, TarefaRepository

--- sgp-api/src/main/java/br/com/treina/recife/sgp/api/repository/ProjetoRepository.java
+++ sgp-api/src/main/java/br/com/treina/recife/sgp/api/repository/ProjetoRepository.java
@@ -2,8 +2,8 @@ package br.com.treina.recife.sgp.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.treina.recife.sgp.api.model.Usuario;
+import br.com.treina.recife.sgp.api.model.Projeto;
 
-public interface ProjetoRepository extends JpaRepository<Usuario, Long> {
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
     
 }
