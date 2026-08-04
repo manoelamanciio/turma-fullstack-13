df4fca86e7dc29c18a86536e67f3d1cb60ea83d8
Manoel Amancio <manoel.amanciio@gmail.com>
2026-08-04 20:26:05 -0300

    Adicionando método tratarCamposInvalidor na linha 33

--- sgp-api/src/main/java/br/com/treina/recife/sgp/api/exceptions/GloblalExceptionHandler.java
+++ sgp-api/src/main/java/br/com/treina/recife/sgp/api/exceptions/GloblalExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,17 +16,33 @@ import br.com.treina.recife.sgp.api.dto.ErroValidacao.CampoInvalido;
 @RestControllerAdvice
 public class GloblalExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErroValidacao> tratarDataInvalida(HttpMessageNotReadableException ex){
+    public ResponseEntity<ErroValidacao> tratarDataInvalida(HttpMessageNotReadableException ex) {
         List<CampoInvalido> campos = new ArrayList<>();
 
         campos.add(new CampoInvalido("Data de Nascimento", "O formato deve ser YYYY-MM-DD"));
 
         ErroValidacao erro = new ErroValidacao(
-            LocalDateTime.now(),
-            HttpStatus.BAD_REQUEST.value(),
-            "Campo invalido",
-            campos
-        );
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Campo invalido",
+                campos);
         return ResponseEntity.badRequest().body(erro);
-    }    
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroValidacao> tratarCamposInvalidos(MethodArgumentNotValidException ex) {
+        List<CampoInvalido> campos = ex.getFieldErrors().stream()
+                .map(erro -> new CampoInvalido(erro.getField(), erro.getDefaultMessage())).toList();
+
+        ErroValidacao erro = new ErroValidacao(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Campos Invalidos",
+                campos
+
+        );
+
+        return ResponseEntity.badRequest().body(erro);
+    }
+
 }
